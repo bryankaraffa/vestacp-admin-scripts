e96f7373231d7eecb493e172f4396fdd1e5f70c9
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-07 20:16:08 -0800

    Added Magento.  Uses same methods as wordpress script, different .tar.gz package, but that is all I changed this time.  Also as some leftoverwork on a concrete5 plugin which (I believe) should work as well.
    	modified:   create-concrete5-site.sh
    	new file:   create-magento-site.sh

--- create-magento-site.sh
+++ create-magento-site.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 #
-# create-concrete5-site.sh
+# create-magento-site.sh
 #
-# Automatically creates a domain, DB, and installs the latest version of concrete5.
+# Automatically creates a domain, DB, and installs the latest version of magento.
 
 vst_newdomain="$1"
 vst_newdomain_ip="$2"
@@ -23,11 +23,11 @@ if [ -z "$vst_password" ]; then
   read -s vst_password
 fi
 if [ -z "$vst_newdomain" ]; then
-  echo "Desired domain name for concrete5 site, followed by [ENTER]:"
+  echo "Desired domain name for Magento site, followed by [ENTER]:"
   read vst_newdomain
 fi
 
-echo '[create-concrete5-site] Creating domain via API call'
+echo '[create-magento-site] Creating domain via API call'
 ## Execute API Call to create domain
 curl -s -X POST https://$vst_hostname:8083/api/ \
 -d user="$vst_username" \
@@ -38,7 +38,7 @@ curl -s -X POST https://$vst_hostname:8083/api/ \
 -d arg2="$vst_newdomain" \
 -d arg3="$vst_newdomain_ip"
 
-echo '[create-concrete5-site] Rebuilding the web configs via API call'
+echo '[create-magento-site] Rebuilding the web configs via API call'
 ## Execute API Call to rebuild web configs
 curl -s -X POST https://$vst_hostname:8083/api/ \
 -d user="$vst_username" \
@@ -49,26 +49,24 @@ curl -s -X POST https://$vst_hostname:8083/api/ \
 
 vst_newdomain_publichtml="~/web/$vst_newdomain/public_html"
 
-c5_release=74619
+echo '[create-magento-site] Downloading magento latest.tar.gz'
+curl -s -O http://www.magentocommerce.com/downloads/assets/1.9.1.0/magento-1.9.1.0.tar.gz
 
-echo '[create-concrete5-site] Downloading concrete5 archive'
-curl -s -O http://www.concrete5.org/download_file/-/view/$c5_release
+echo '[create-magento-site] Unpacking magento latest.tar.gz'
+tar -zxf magento-1.9.1.0.tar.gz
 
-echo '[create-concrete5-site] Unpacking concrete5 archive'
-tar -zxf $c5_release
-
-echo "[create-concrete5-site] Uploading wordpress files to ~/web/$vst_newdomain/public_html"
-rsync -e ssh -azh concrete5*/* $vst_username@$vst_hostname:$vst_newdomain_publichtml
+echo "[create-magento-site] Uploading magento files to ~/web/$vst_newdomain/public_html"
+rsync -e ssh -azh magento/* $vst_username@$vst_hostname:$vst_newdomain_publichtml
 
 # Cleanup
-rm -rf $c5_release concrete5*/
+rm -rf magento-1.9.1.0.tar.gz magento
 
-echo "[create-concrete5-site] Removing $vst_newdomain_publichtml/index.html"
+echo "[create-magento-site] Removing $vst_newdomain_publichtml/index.html"
 ssh $vst_username@$vst_hostname "rm $vst_newdomain_publichtml/index.html"
 
-echo "[create-concrete5-site] Creating Database via API call"
+echo "[create-magento-site] Creating Database via API call"
 ## Generate DB Name
-mysql_db_name="c5_${vst_newdomain//./}"
+mysql_db_name="wp_${vst_newdomain//./}"
 ## Generate MySQL Username
 mysql_username=$vst_username"_"$mysql_db_name
 #### Shorten the MySQL username to make sure it's shorter than 16 chars (including the username)
@@ -88,13 +86,13 @@ curl -s -X POST https://$vst_hostname:8083/api/ \
 -d arg5="mysql"
 
 
-echo "[create-concrete5-site] ********************************************"
-echo "[create-concrete5-site] MySQL DB Credentials"
-echo "[create-concrete5-site] Database: $vst_username"_"$mysql_db_name"
-echo "[create-concrete5-site] Username: $vst_username"_"$mysql_username"
-echo "[create-concrete5-site] Password: $mysql_password"
-echo "[create-concrete5-site] ********************************************"
+echo "[create-magento-site] ********************************************"
+echo "[create-magento-site] MySQL DB Credentials"
+echo "[create-magento-site] Database: $vst_username"_"$mysql_db_name"
+echo "[create-magento-site] Username: $vst_username"_"$mysql_username"
+echo "[create-magento-site] Password: $mysql_password"
+echo "[create-magento-site] ********************************************"
 
-echo "[create-concrete5-site] Completed!"
-echo "[create-concrete5-site] You can now visit the website to finish the installation."
+echo "[create-magento-site] Completed!"
+echo "[create-magento-site] You can now visit the website to finish the installation."
 exit 0
